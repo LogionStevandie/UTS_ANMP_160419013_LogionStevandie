547867e7529701c91b586ac602848cd3dfb43d94
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-09-13 13:59:55 +0700

    View (Fragment sudah 8), Model (City), ProfileDetail UI Sudah

---
+++ app/src/main/java/id/ac/ubaya/informatika/a160419013_vaccinecovid19/model/Model.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.informatika.a160419013_vaccinecovid19.model
+
+data class City(
+    val id:String?,
+    val name:String?,
+    val zona:String?,
+    val positiv:String?,
+    val negativ:String?,
+    val sembuh:String?,
+    val meninggal:String?
+)
