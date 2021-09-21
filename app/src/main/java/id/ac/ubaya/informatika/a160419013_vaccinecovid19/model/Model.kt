8fe5061e1b07a9ce40c3f9352a78ddc243f83234
LogionStevandie <s160419013@student.ubaya.ac.id>
2021-09-21 20:10:10 +0700

    login, navigation, model nambah resiko

--- app/src/main/java/id/ac/ubaya/informatika/a160419013_vaccinecovid19/model/Model.kt
+++ app/src/main/java/id/ac/ubaya/informatika/a160419013_vaccinecovid19/model/Model.kt
@@ -3,6 +3,7 @@ package id.ac.ubaya.informatika.a160419013_vaccinecovid19.model
 data class City(
     val id:String?,
     val name:String?,
+    val resiko:String?,
     val zona:String?,
     val positiv:String?,
     val negativ:String?,
---
+++ app/src/main/java/id/ac/ubaya/informatika/a160419013_vaccinecovid19/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ubaya.informatika.a160419013_vaccinecovid19.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ubaya.informatika.a160419013_vaccinecovid19.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
